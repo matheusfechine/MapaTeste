859be399b5674e5439f47809640e358462ee7337
matheus <matheus@matheus-MS-7529.(none)>
2013-07-17 18:04:05 -0300

    Refactor do JS do Mapa

--- src/main/webapp/javascript/mapa.js
+++ src/main/webapp/javascript/mapa.js
@@ -1,41 +1,57 @@
 var Mapa;
 (function($){
 	Mapa = {};
-	var vectors, controls, map, wms;	
+	var vectors, controls, map, wms, renderer, control;	
 	Mapa.renderizaMapa = function(){
-		map = new OpenLayers.Map('map');
-		wms = new OpenLayers.Layer.WMS("OpenLayers WMS",
-				"http://vmap0.tiles.osgeo.org/wms/vmap0", {
-					layers : 'basic'
-				});
-		
-		var renderer = OpenLayers.Util.getParameters(window.location.href).renderer;
-        renderer = (renderer) ? [renderer] : OpenLayers.Layer.Vector.prototype.renderers;
-        vectors = new OpenLayers.Layer.Vector("Vector Layer", {
-            renderers: renderer
-        });
-        
-		map.addLayers([wms, vectors]);
-		
-		map.addControl(new OpenLayers.Control.LayerSwitcher());
-        map.addControl(new OpenLayers.Control.MousePosition());
-        controls = {
-                point: new OpenLayers.Control.DrawFeature(vectors,
-                            OpenLayers.Handler.Point),
-                line: new OpenLayers.Control.DrawFeature(vectors,
-                            OpenLayers.Handler.Path),
-                polygon: new OpenLayers.Control.DrawFeature(vectors,
-                            OpenLayers.Handler.Polygon),
-                drag: new OpenLayers.Control.DragFeature(vectors)
-            };
-        for(var key in controls) {
-            map.addControl(controls[key]);
-        }
-        var control = controls.polygon;
-        control.activate();
+		Mapa.instanciaMapa();
+		Mapa.instanciaRenderizadores();
+        Mapa.adicionaLayers();
+        Mapa.adicionaControls();
+        Mapa.ativaDesenhoDePolygon();
 		map.zoomToMaxExtent();
 	};
+
+Mapa.instanciaMapa = function(){
+	map = new OpenLayers.Map('map');
+	wms = new OpenLayers.Layer.WMS("OpenLayers WMS",
+			"http://vmap0.tiles.osgeo.org/wms/vmap0", {
+				layers : 'basic'
+			});
+}	
+Mapa.instanciaRenderizadores = function(){
+	renderer = OpenLayers.Util.getParameters(window.location.href).renderer;
+    renderer = (renderer) ? [renderer] : OpenLayers.Layer.Vector.prototype.renderers;
+    vectors = new OpenLayers.Layer.Vector("Vector Layer", {
+        renderers: renderer
+    });
+}	
+Mapa.adicionaLayers = function(){
+	map.addLayers([wms, vectors]);
+}
+
+Mapa.adicionaControls = function(){
+	map.addControl(new OpenLayers.Control.LayerSwitcher());
+    map.addControl(new OpenLayers.Control.MousePosition());
+    controls = {
+            point: new OpenLayers.Control.DrawFeature(vectors,
+                        OpenLayers.Handler.Point),
+            line: new OpenLayers.Control.DrawFeature(vectors,
+                        OpenLayers.Handler.Path),
+            polygon: new OpenLayers.Control.DrawFeature(vectors,
+                        OpenLayers.Handler.Polygon),
+            drag: new OpenLayers.Control.DragFeature(vectors)
+        };
+    for(var key in controls) {
+        map.addControl(controls[key]);
+    }
+}	
+Mapa.ativaDesenhoDePolygon = function(){
+	control = controls.polygon;
+	control.activate();
+}
+	
 Mapa.renderizaMapa();
+
 }(jQuery));
 
 
